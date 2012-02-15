042de78d27da2fef868d0da10af1667106dd8aa2
Josh <jbonnell@uark.edu>
2012-02-15 17:02:54 -0600

    background,spaceship, etc with comments

---
+++ ProjectCow/ProjectCow/ProjectCow/Background.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace WindowsGame2
+{
+    class Background
+    {
+        //The current position of the Sprite
+        public Vector2 Position = new Vector2(0,0);
+
+        //The texture object used when drawing the sprite
+        private Texture2D mSpriteTexture;
+
+        string[] names = new string[3];
+        string name;
+        int num;
+
+        public Background()
+        {
+            names[0] = "farm1";
+            names[1] = "farm2";
+            names[2] = "farm3";
+
+            num = 0;
+            name = names[0];
+        }
+
+        //Load the texture for the sprite using the Content Pipeline
+        public void LoadContent(ContentManager theContentManager)
+        {
+            mSpriteTexture = theContentManager.Load<Texture2D>(names[num]);
+        }
+
+        //Draw the sprite to the screen
+        public void Draw(SpriteBatch theSpriteBatch)
+        {
+            theSpriteBatch.Draw(mSpriteTexture, Position, Color.White);
+        }
+
+        public void setBackgroundImage()
+        {
+            Random random = new Random();
+            num = random.Next(0, 3);
+        }
+
+        public string getBackgroundImage()
+        {
+            return names[num];
+        }
+    }
+}
---
+++ ProjectCow/ProjectCowTest/SpaceshipTest.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using ProjectCow;
+
+namespace ProjectCowTest
+{
+    [TestClass]
+    public class SpaceshipTest
+    {
+        [TestMethod]
+        public void MoveLeftTest()
+        {
+            Spaceship spaceship = new Spaceship();
+            spaceship.X = 300;
+
+            spaceship.MoveLeft();
+
+            Assert.AreEqual(300 - spaceship.Speed, spaceship.X);
+        }
+
+        [TestMethod]
+        public void MoveRightTest()
+        {
+            Spaceship spaceship = new Spaceship();
+            spaceship.X = 0;
+
+            spaceship.MoveRight();
+
+            Assert.AreEqual(0 + spaceship.Speed, spaceship.X);
+        }
+
+        [TestMethod]
+        public void MoveUpTest()
+        {
+            Spaceship spaceship = new Spaceship();
+            spaceship.Y = 300;
+
+            spaceship.MoveUp();
+
+            Assert.AreEqual(300 - spaceship.Speed, spaceship.Y);
+        }
+
+        [TestMethod]
+        public void MoveDownTest()
+        {
+            Spaceship spaceship = new Spaceship();
+            spaceship.Y = 0;
+
+            spaceship.MoveDown();
+
+            Assert.AreEqual(0 + spaceship.Speed, spaceship.Y);
+        }
+
+        [TestMethod]
+        public void LeftBoundaryTest()
+        {
+            Spaceship spaceship = new Spaceship();
+            spaceship.LeftBoundary = 0;
+            spaceship.MoveLeft();
+
+            Assert.AreEqual(0, spaceship.X);
+        }
+
+        [TestMethod]
+        public void RightBoundaryTest()
+        {
+            Spaceship spaceship = new Spaceship();
+            spaceship.X = 300 - spaceship.Width;
+            spaceship.RightBoundary = 300;
+            spaceship.MoveRight();
+
+            Assert.AreEqual(300 - spaceship.Width, spaceship.X);
+        }
+
+        [TestMethod]
+        public void TopBoundaryTest()
+        {
+            Spaceship spaceship = new Spaceship();
+            spaceship.Y = 0;
+            spaceship.TopBoundary = 0;
+            spaceship.MoveUp();
+
+            Assert.AreEqual(0, spaceship.Y);
+        }
+
+        [TestMethod]
+        public void BottomBoundaryTest()
+        {
+            Spaceship spaceship = new Spaceship();
+            spaceship.Y = 300 - spaceship.Height;
+            spaceship.BottomBoundary = 300;
+            spaceship.MoveDown();
+
+            Assert.AreEqual(300 - spaceship.Height, spaceship.Y);
+        }
+    }
+}
