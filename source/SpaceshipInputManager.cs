92029424a384c545cb23b59506b960e4899e52af
Hieu Truong <httruong@uark.edu>
2012-02-15 17:43:12 -0600

    organizing

---
+++ source/SpaceshipInputManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace ProjectCow
+{
+    public class SpaceshipInputManager
+    {
+        Spaceship spaceship;
+
+        public SpaceshipInputManager(Spaceship spaceship)
+        {
+            this.spaceship = spaceship;
+        }
+
+        public void ProcessInputs(KeyboardState state)
+        {
+            if (state.IsKeyDown(Keys.A))
+                spaceship.MoveLeft();
+            else if (state.IsKeyDown(Keys.D))
+                spaceship.MoveRight();
+
+            if (state.IsKeyDown(Keys.W))
+                spaceship.MoveUp();
+            else if (state.IsKeyDown(Keys.S))
+                spaceship.MoveDown();
+
+            if (state.IsKeyDown(Keys.Space))
+                spaceship.TractorBeam = true;
+            else
+                spaceship.TractorBeam = false;
+        }
+
+        public void Update()
+        {
+            ProcessInputs(Keyboard.GetState());
+        }
+    }
+}
